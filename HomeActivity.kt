d39bd630bbca18a3b4969ed7055ac839e1be04de
Naufal <muhnaufal@students.amikom.ac.id>
2025-10-18 16:39:38 +0700

    Create HomeActivity.kt

---
+++ HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.example.a3129_myapplication1
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home) 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val webButton = findViewById<Button>(R.id.open_web_button)
+        webButton.setOnClickListener {
+            val url = "https://amikom.ac.id"
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+
+            startActivity(intent)
+        }
+    }
+}
